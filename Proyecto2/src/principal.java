7ed089df6315d18e000eb361eba25181c2533198
neptu <neptu@192.168.1.134>
2024-04-10 18:00:34 +0200

    Tercer Commit

--- Proyecto2/src/principal.java
+++ Proyecto2/src/principal.java
@@ -4,6 +4,7 @@ public class principal {
 	public static void main(String[] args) {
 		System.out.println("¡Hola, Mundo!");
 		System.out.println("Adiós,");
+		System.out.println("Nos vemos");
 	}
 
 }
