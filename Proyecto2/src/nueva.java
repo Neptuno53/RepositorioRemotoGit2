f8cc44847b8b6c3d18ebd29de6f8cf50b88bf565
neptu <neptu@192.168.1.134>
2024-04-10 20:56:15 +0200

    Commit con 2 mensajes nuevos

--- Proyecto2/src/nueva.java
+++ Proyecto2/src/nueva.java
@@ -4,6 +4,8 @@ public class nueva {
 	public static void main(String[] args) {
 		System.out.println("Hola,");
 		System.out.println("Estamos utilizando una clase nueva");
+		System.out.println("1 Mensaje Nuevo");
+		System.out.println("2 Mensajes Nuevos");
 	}
 
 }
